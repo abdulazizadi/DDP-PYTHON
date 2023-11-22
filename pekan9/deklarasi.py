d0eaa79a0ae36e292d490b2158eda0a7c2ea66d7
abdulazizadi <146091146+abdulazizadi@users.noreply.github.com>
2023-11-22 12:01:22 +0700

    Add files via upload

---
+++ pekan9/deklarasi.py
@@ -0,0 +1,28 @@
+def hello():
+    print("hello saya abdul aziz adi")
+    print("hallo stt nf")
+
+def panggil():
+    hello()
+    print("saya ti03")
+
+def say(nama):
+    print("hello nama saya adalah",nama)
+
+def jumlah(a,b):
+    jumlah = a+b
+    print(jumlah)
+
+def pengali (x):
+    return x*2
+
+def hitungluas(sisi):
+    luas= sisi*sisi
+    return luas
+
+def pangkat(a,b):
+    return a**b
+
+     #memanggil fungsi
+
+print (hitungluas(5))
---
+++ pekan9/tugas9.3.py
@@ -0,0 +1,7 @@
+def ganjil(n):
+    print("Bilangan Ganjil dari 1 sampai", n, "adalah:")
+    for i in range(1, n + 1,2):
+        print(i)
+
+# pemanggilan fungsi
+ganjil(100)
